6297c80d0dac786ef7e5a7137c505df1316f7580
Liam Knapp <Lknapp1110@conestogac.on.ca>
2023-01-18 11:31:32 -0500

    Added Project files

---
+++ Client Side/Client Side/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Client_Side
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
