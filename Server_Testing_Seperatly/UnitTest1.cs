7f2500211a4241553d957f99319582fdf3c4d593
Liam Knapp <Lknapp1110@conestogac.on.ca>
2023-04-04 18:33:01 -0400

    Added testing for server and added usability tests, integration tests, system tests that were manually explored and documented results

--- Server_Testing_Seperatly/UnitTest1.cs
+++ Server_Testing_Seperatly/UnitTest1.cs
@@ -20,5 +20,52 @@ namespace Server_Testing_Seperatly
             // Assert
             Assert.IsFalse(result);
         }
+
+        /*LIAM'S USABILITY TESTS: DONT TOUCH UNTIL I MOVES IT TO DOC
+         *MANUAL TESTED
+        TESTING_SCREEN_RESOLUTION = PASSED
+        TESTING_CLIENT_TO_SERVER_RESPONSE_TIME = time elapsed to transfer propmts averages 0.00001 ms when rounded  (ran the test 30 times) = PASSED
+        TESTING_THE_TRANSITIONS_SMOOTH_PAGES = pages all work interactively with litlle delay, no noticable bugs with transitions and responsiveness = PASSED
+        TESTING_TRANSITIONS_BETWEEN_PAGES_CLIENT =  the pages all work as intended includeing the slide out menu = PASSED
+        TESTING_DATE_TIME_CORRECTION = the server is running in real time  = PASSED
+        TESTING_THE_COMPATABILITY_ANDROID = 
+        TESTING_THE_SCREEN_RESOLUTION_ANDROID = 
+
+         **LIAM'S INTEGRATION TESTS: DONT TOUCH UNTIL I MOVE IT TO DOC
+         *MANUAL TESTED
+        TESTING_PAGES_ROUTING_MAINMENU_LOGIN = the pages were able to switch to the mainmenu from the login after the correct info was parsed = PASSED
+
+         **LIAM'S SYSTEM TESTS: DONT TOUCH UNTIL I MOVE IT TO DOC
+         *MANUAL TESTED
+        TESTING_ENCRYPTED_REQUEST = the server is able to decrypt the prompts correctly = PASSED
+        TESTING_BLACKLIST_CLIENT_ADD_FUNCTION = the client does get blacklisted from the server connection correctly (multiple clients can also be blacklisted correctly) = PASSED
+
+        */
+
+        [TestMethod]
+        public void TEST_SERVER_API_OPENAI()
+        {
+            // Arrange
+            string expected = "Paris";
+            string prompt = "Whats the capital of France?";
+
+
+            // Act
+            string result = AI_API.callOpenAIText(prompt);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TEST_GENERATE_SOCKET()
+        {
+            // Arrange
+           
+            //ACT
+
+            // Assert
+
+        }
     }
 }
